7575e82aaaab61a8fbb6199ef04dc304a5e7ac6c
Matt Guthre <theindigobricks@gmail.com>
2022-08-06 14:16:41 -0400

    Annual Display Window completely Finished

--- src/main/java/com/example/invoicetracker/AnnualProfitsController.java
+++ src/main/java/com/example/invoicetracker/AnnualProfitsController.java
@@ -1,11 +1,9 @@
 package com.example.invoicetracker;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Row;
@@ -15,18 +13,21 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.Iterator;
 
 public class AnnualProfitsController {
-    FileInputStream invoiceFilePath = new FileInputStream("C:\\Users\\MGU31\\OneDrive\\Desktop\\Invoice file .xlsx");
+    FileInputStream invoiceFilePath = new FileInputStream("C:\\Users\\MGU31\\Invoice file .xlsx");
     @FXML
     private Button refresh;
 
     @FXML
-    private Button done;
+    private TableView annualTable;
 
     @FXML
-    private TableView annualTable;
+    private Label yearsProfit,yearsPieces,yearsLots;
+
+
 
     @FXML
     private TableColumn<AnnualInvoice, String> invoiceIDColumn;
@@ -55,24 +56,39 @@ public class AnnualProfitsController {
 
         DataFormatter formatter = new DataFormatter();
         FormulaEvaluator evaluator = invoiceWorkbook.getCreationHelper().createFormulaEvaluator();
+        DecimalFormat df = new DecimalFormat("#.##");
+
+
+        int counter = 0;
+        int totalLots = 0;
+        int totalPieces = 0;
+        double entireTotalMade = 0.00;
 
         Iterator<Row> rowIterator = invoiceSheet.iterator();
         rowIterator.next();
         while(rowIterator.hasNext()){
             Row row = rowIterator.next();
-            String invoiceID = formatter.formatCellValue(row.getCell(3));
-            String totalMade = formatter.formatCellValue(row.getCell(9), evaluator);
-            String lots = formatter.formatCellValue(row.getCell(2));
-            String pieces = formatter.formatCellValue(row.getCell(1));
+            if (row.getCell(3 )!= null && row.getCell(3).getCellType() != CellType.BLANK) {
+                String invoiceID = formatter.formatCellValue(row.getCell(3));
+                String totalMade = formatter.formatCellValue(row.getCell(9), evaluator);
+                String lots = formatter.formatCellValue(row.getCell(2));
+                String pieces = formatter.formatCellValue(row.getCell(1));
 
-            AnnualInvoice invoice = new AnnualInvoice(invoiceID,lots,pieces,totalMade);
-            annualTable.getItems().add(invoice);
+                int tempLots = Integer.parseInt(lots);
+                totalLots = totalLots + tempLots;
+
+                int tempPieces = Integer.parseInt(pieces);
+                totalPieces = totalPieces + tempPieces;
+
+                double tempTotalMade = row.getCell(9).getNumericCellValue();
+                entireTotalMade = entireTotalMade + tempTotalMade;
+
+                AnnualInvoice invoice = new AnnualInvoice(invoiceID,lots,pieces,totalMade);
+                annualTable.getItems().add(invoice);
+            }
         }
+        yearsLots.setText(String.valueOf(totalLots));
+        yearsPieces.setText(String.valueOf(totalPieces));
+        yearsProfit.setText("$" + String.valueOf(df.format(entireTotalMade)));
     }
-
-    @FXML
-    void onDoneClick(){
-
-    }
-
 }
